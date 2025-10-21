08c3ed6bf895817b68d30a11a0a848e4163036a5
Nagalla Srinivasa Raju <theinfiniteknowledgeheist@gmail.com>
2025-10-21 13:48:59 +0530

    Deploy

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ from routers import VocabularyPillRouters
 
 app = FastAPI()
 
+# Create database tables
 models.Base.metadata.create_all(bind=engine)
+
+
 app.include_router(VocabularyPillRouters.router)
 
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8080)
